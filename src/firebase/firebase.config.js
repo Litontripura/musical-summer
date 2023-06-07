50dd70b88805489b2a0dd2d01f1932f08d8eab5c
Litontripura <litontripura01@gmail.com>
2023-06-07 11:00:02 +0600

    i have finished the navbar

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSrj6hsv_5yb1Qy-SiZ3gkZgbn4L67m54",
+  authDomain: "summer-camp-school-1c113.firebaseapp.com",
+  projectId: "summer-camp-school-1c113",
+  storageBucket: "summer-camp-school-1c113.appspot.com",
+  messagingSenderId: "1074279257055",
+  appId: "1:1074279257055:web:260928ae64743969256a6a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
