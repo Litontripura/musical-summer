f526669135e672782597615353fdacb37e05774c
Litontripura <litontripura01@gmail.com>
2023-06-07 23:22:59 +0600

    i have created registration form

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,34 +1,54 @@
 
 
 const Registration = () => {
+        const handleSubmit =(event)=>{
+            event.preventDefault()
+            const form = event.target;
+            const name = form.name.value;
+            const email = form.email.value;
+            const password = form.password.value;
+            const confirmPass = form.confirmPass.value;
+            console.log(name, email, password, confirmPass);
+        }
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="text-5xl font-bold">Create account to Creative Summer</h1>
+      
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <form onSubmit={handleSubmit} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input name="name" type="text" placeholder="Enter your Name" className="input input-bordered" />
+        </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input name="email" type="email" placeholder="Enter your email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input name="password" type="password" placeholder="password" className="input input-bordered" />
+        
+        </div>
+        <div className="form-control">
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <span className="label-text">Confirm Password</span>
           </label>
+          <input name="confirmPass" type="password" placeholder="Enter your confirm password" className="input input-bordered" />
+        
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <input type="submit" value="Register" />
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </div>
