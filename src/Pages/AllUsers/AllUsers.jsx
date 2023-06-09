14e7aa0f881d726f583c7b741b0f3a97613478d1
Litontripura <litontripura01@gmail.com>
2023-06-09 16:27:02 +0600

    i have done the admin and instructor button conditional

--- src/Pages/AllUsers/AllUsers.jsx
+++ src/Pages/AllUsers/AllUsers.jsx
@@ -1,15 +1,44 @@
 import { useQuery } from "@tanstack/react-query";
+import { AiTwotoneDelete } from "react-icons/ai";
+import "./alluser.css";
+import toast, { Toaster } from 'react-hot-toast';
 
 const AllUsers = () => {
-  const { data: users = [], refetch } = useQuery(["users"],async()=>{
+  const notify = () => toast('role updated successfully');
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
     const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
   console.log(users);
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PATCH"
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.modifienCount) {
+          notify()
+        }
+      })
+
+  };
+  const handleMakeInstructor = (id) => {
+    fetch(`http://localhost:5000/users/instructor/${id}`, {
+      method: "PATCH"
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.modifienCount) {
+          notify()
+        }
+      })
+  };
+
   return (
     <div className="overflow-x-auto text-primary">
       <p>{users.length}</p>
-      <table className="table ">
+      <Toaster />
+      <table className="table radius text-center rounded-lg">
         {/* head */}
         <thead className="text-primary">
           <tr>
@@ -18,6 +47,7 @@ const AllUsers = () => {
             <th>email</th>
             <th>role</th>
             <th></th>
+            <th>delete user</th>
           </tr>
         </thead>
         <tbody>
@@ -26,19 +56,37 @@ const AllUsers = () => {
               <th>{index + 1}</th>
               <td>
                 <div className="flex items-center space-x-3">
-                
                   <div>
                     <div className="font-bold">{user.name}</div>
-                   
                   </div>
                 </div>
               </td>
-              <td>
-             {user.email}
-              </td>
+              <td>{user.email}</td>
               <td>{user.role}</td>
+              <td>
+                {user.role === "admin" ? (
+                  <div className="badge badge-primary cursor-pointer text-white bg-gray-300" disabled>
+                    make admin
+                  </div>
+                ) : (
+                  <div onClick={() => handleMakeAdmin(user._id)} className="badge badge-primary cursor-pointer text-white">
+                    make admin
+                  </div>
+                )}
+                {user.role === "instructor" ? (
+                  <div className="badge badge-primary ml-2 text-white bg-gray-300 cursor-pointer" disabled>
+                    make instructor
+                  </div>
+                ) : (
+                  <div onClick={() => handleMakeInstructor(user._id)} className="badge badge-primary ml-2 text-white cursor-pointer">
+                    make instructor
+                  </div>
+                )}
+              </td>
               <th>
-                <button className="btn btn-primary text-white">delete</button>
+                <button className="btn-accent p-1 rounded-full text-2xl text-orange-700">
+                  <AiTwotoneDelete></AiTwotoneDelete>
+                </button>
               </th>
             </tr>
           ))}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import  { Toaster } from 'react-hot-toast';
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 import AuthProviders from "./Providers/AuthProviders.jsx";
@@ -9,6 +10,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -16,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
    <AuthProviders>
     <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+   
     <RouterProvider router={router} />
     </QueryClientProvider>
      
