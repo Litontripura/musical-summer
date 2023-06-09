da2ac1419822c00d0db6f121e1e12f73a1e2ebc8
Litontripura <litontripura01@gmail.com>
2023-06-09 17:36:44 +0600

    i have created the classes card this time

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClassCard from './ClassCard';
 
 const Classes = () => {
+    const [classes, setClasses]=useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/classes')
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            setClasses(data)
+        })
+    },[])
     return (
-        <div>
-            <h1>This is Classes</h1>
+        <div className='mx-auto'>
+            <h1>{classes.length}</h1>
+          <div className='grid grid-cols-2 justify-center items-center mx-auto'>
+          {
+            classes.map(item=>
+                <ClassCard
+                key={item._id}
+                item={item}
+                ></ClassCard>
+            )
+           }
+          </div>
         </div>
     );
 };
